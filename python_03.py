e97eacf2936b8d644e22de30ea322e93398df8a8
David Bernal <dbernal@outpostqa.com>
2023-02-14 19:16:04 -0700

    aversicierto

---
+++ python_03.py
@@ -0,0 +1,8 @@
+nombre = input("Cómo tú te shama?... ")
+edad = input("Cuántos años tenés vos?... ")
+ciudad = input("Dónde vive usté?... ")
+casado = input("Estás tú casao?... ")
+numA = input("Piensa en un número de 1 a 100... ")
+numbB = input("Ahora un número de 100 a 200... ")
+print(f'"Mi nombre es {nombre}, mi edad es {edad} años, '
+      f'vivo en {ciudad}, mi resultado es {int(numA)/int(numbB)}')
