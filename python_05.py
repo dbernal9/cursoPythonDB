f9b9afbd4c408a9e8071610e904da49a22c38f77
David Bernal <dbernal@outpostqa.com>
2023-02-16 18:26:43 -0700

    Ejercicio modulo 5 | David Bernal

--- python_05.py
+++ python_05.py
@@ -1,3 +1,6 @@
+"""1. Pide al usuario dos variables a = 12.5 y b = 34, crea funciones que permitan calcular la suma, resta,
+multiplicación y división, como también el valor del módulo de b entre a"""
+
 def suma(numA, numB):
     return (numA+numB)
 def resta(numA: int, numB: int):
@@ -18,8 +21,8 @@ print("La multiplicacion de tus numeros es ", multip(numero_a,numero_b))
 print("La division de tus numeros es ", divi(numero_a,numero_b))
 print("El modulo de tus numeros es ", modulo(numero_a,numero_b))
 
-#######################################################################
-
+########################################################################################################################
+"""2. Crea un método que permita convertir cualquier numero entero y a flotante"""
 def int2float(num: int):
     return float(num)
 
@@ -27,8 +30,9 @@ numero_entero = int(input("Dame un numero entero: "))
 print("Tu numero en float se ve asi: ", int2float(numero_entero))
 
 
-#######################################################################
-
+########################################################################################################################
+"""Extra: Define una función para convertir de grados Celsius a Fahrenheit, 
+pide al usuario que ingrese la temperatura en Celsius e imprima la conversión."""
 def celcius2fahrenheit(temperature):
     t = temperature*(9/5)+32
     return t
